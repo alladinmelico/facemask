83d721e91f7111d2d00409eb8916bfb7b944294e
Alladin Melico <melico.alladin@gmail.com>
2020-12-14 10:51:04 +0800

    inertia initialized

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,32 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
+const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
-    purge: [
-        './vendor/laravel/jetstream/**/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
-        './resources/js/**/*.vue',
-    ],
+	purge: [
+		'./vendor/laravel/jetstream/**/*.blade.php',
+		'./storage/framework/views/*.php',
+		'./resources/views/**/*.blade.php',
+		'./resources/js/**/*.vue'
+	],
 
-    theme: {
-        extend: {
-            fontFamily: {
-                sans: ['Nunito', ...defaultTheme.fontFamily.sans],
-            },
-        },
-    },
+	theme: {
+		extend: {
+			colors: {
+				primary: '#F5DF4D',
+				secondary: '#939597',
+				'light-secondary': '#DCE0E3',
+				'dark-secondary': '#606263',
+				'light-primary': '#635A1F',
+				'dark-primary': '#E3D78A'
+			},
+			fontFamily: {
+				sans: ['Nunito', ...defaultTheme.fontFamily.sans]
+			}
+		}
+	},
 
-    variants: {
-        opacity: ['responsive', 'hover', 'focus', 'disabled'],
-    },
+	variants: {
+		opacity: ['responsive', 'hover', 'focus', 'disabled']
+	},
 
-    plugins: [require('@tailwindcss/ui')],
-};
+	plugins: [require('@tailwindcss/ui')]
+}
