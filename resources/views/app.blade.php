466c589dd912329156610338cf47b0aff892d287
Alladin Melico <melico.alladin@gmail.com>
2021-01-03 17:31:57 +0800

    moved photo to public

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -14,7 +14,7 @@
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
         <link href="https://cdn.jsdelivr.net/npm/@mdi/font@4.x/css/materialdesignicons.min.css" rel="stylesheet">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css" />
-        <link rel="shortcut icon" href="{{ url('storage/img/icon.ico') }}">
+        <link rel="shortcut icon" href="/img/icon.ico">
         <!-- Scripts -->
         @routes
         <script src="{{ mix('js/app.js') }}" defer></script>
